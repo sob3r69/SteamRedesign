f2da4ec563238fd8e5a1cad30b1a06910d764385
sob3r69 <iamnotsob3r@gmail.com>
2023-11-24 15:49:49 +0300

    feature: Added dropdown menu to AccountCard And implemented hook that listen the click outside of component

--- src/Widgets/AccountCard/AccountCard.tsx
+++ src/Widgets/AccountCard/AccountCard.tsx
@@ -1,16 +1,44 @@
 import { ArrowDown } from '@/app/assets';
 import './AccountCard.scss';
+import { useEffect, useRef, useState } from 'react';
+
+const useOutsideClick = (callback: (value: boolean) => void) => {
+  const ref = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    console.log('render');
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback(false);
+      }
+    };
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [ref]);
+  return ref;
+};
 
 const AccountCard = () => {
+  const [visibility, setVisibility] = useState(false);
+  const handleClickOutside = () => setVisibility(false);
+
+  const cardRef = useOutsideClick(handleClickOutside);
+
   return (
     <div className="account-container">
-      <button className="account-card">
+      <button className="account-card" ref={cardRef} onClick={() => setVisibility(!visibility)}>
         <div className="account-avatar-placeholder"></div>
         <h5 className="account-nickName">Account</h5>
         <h5 className="account-balance">$3.22</h5>
-        <img src={ArrowDown}></img>
+        <img src={ArrowDown} />
       </button>
-      <section className="account-settings"></section>
+      <section
+        className="account-settings"
+        style={visibility ? { opacity: 1 } : { opacity: 0 }}
+      ></section>
     </div>
   );
 };
