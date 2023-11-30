e52e0d5874a10639086b0709db12b12dd21eb646
sob3r69 <iamnotsob3r@gmail.com>
2023-11-30 17:52:42 +0300

    style: Make some stuff, need to make an api server

--- src/Pages/StorePage.tsx
+++ src/Pages/StorePage.tsx
@@ -4,15 +4,10 @@ const StorePage = () => {
   return (
     <>
       <main>
-        {/* <img
-          height={458}
-          width={766}
-          src="https://cdn.cloudflare.steamstatic.com/steam/apps/2000950/capsule_616x353.jpg?t=1678294805"
-        /> */}
-        <BigGameCard />
-
-        {/* <img src="https://cdn.cloudflare.steamstatic.com/steam/apps/2000950/header.jpg?t=1678294805"></img> */}
-        {/* <img src="https://cdn.cloudflare.steamstatic.com/steam/apps/1272080/capsule_616x353.jpg?t=1700498258"></img> */}
+        <div className="store-featured">
+          <h2>FEATURED</h2>
+          <BigGameCard gameID="632360" />
+        </div>
       </main>
     </>
   );
--- src/Widgets/BigGameCard/BigGameCard.tsx
+++ src/Widgets/BigGameCard/BigGameCard.tsx
@@ -1,14 +1,42 @@
+import axios from 'axios';
 import { GameTag } from '@/shared/components';
 import './BigGameCard.scss';
+import { useEffect } from 'react';
 
-const BigGameCard = () => {
+type BigGameCardProps = {
+  gameID: string;
+};
+
+const BigGameCard = ({ gameID }: BigGameCardProps) => {
+  // useEffect(() => {
+  //   // axios({
+  //   //   method: 'get',
+  //   //   url: 'https://store.steampowered.com/api/appdetails?appids=' + gameID,
+  //   //   withCredentials: true,
+  //   //   headers: {
+
+  //   //   },
+  //   // })
+  //   //   .then((response) => {
+  //   //     console.log(response.data);
+  //   //   })
+  //   //   .catch((error) => {
+  //   //     console.error(error);
+  //   //   });
+
+  //   // fetch('https://store.steampowered.com/api/appdetails?appids=' + gameID, {
+  //   //   mode: 'no-cors',
+  //   // }).then((response) => {
+  //   //   console.log(response);
+  //   // });
+  // }, []);
   return (
     <div className="gamecard_container">
       <img
         className="gamecard_cover"
         width={616}
         height={353}
-        src="https://cdn.cloudflare.steamstatic.com/steam/apps/2000950/capsule_616x353.jpg?t=1678294805"
+        src={'https://cdn.cloudflare.steamstatic.com/steam/apps/' + gameID + '/capsule_616x353.jpg'}
       />
       <div className="gamecard_details">
         <h2>Call of Duty: Modern Warfare</h2>
