c59eb06abe481b6fbb8f43578d743685c8f0a8b2
sob3r69 <iamnotsob3r@gmail.com>
2024-01-25 17:45:02 +0300

    feature[backend]: Implemented feature that scraps top sellers from steam

--- backend/main.go
+++ backend/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/gocolly/colly"
 )
 
 func main() {
@@ -17,6 +18,7 @@ func main() {
 
 	r.GET("/", SendDefaultMSG)
 	r.GET("/app/:id", SendAppData)
+	r.GET("/top_sellers", SendTopSellers)
 
 	r.Run(":3000") 
 }
@@ -34,6 +36,11 @@ func SendAppData(c *gin.Context) {
 	c.JSON(200, data.(map[string]interface{})[id].(map[string]interface{})["data"])
 }
 
+func SendTopSellers(c *gin.Context) {
+	var topData = getTopSellers()
+	c.JSON(200, topData)
+}
+
 func getJson(url string, target interface{}) error {
 	r, err := http.Get(url)
 	if err != nil {
@@ -42,4 +49,16 @@ func getJson(url string, target interface{}) error {
 	defer r.Body.Close()
 
 	return json.NewDecoder(r.Body).Decode(target)
+}
+
+
+func getTopSellers() []string {
+	var result = []string{}
+	c := colly.NewCollector()
+
+	c.OnHTML("#search_resultsRows .search_result_row", func(e *colly.HTMLElement) {
+		result = append(result, (e.Attr("data-ds-appid")))
+	})
+	c.Visit("https://store.steampowered.com/search/?filter=topsellers")
+	return result
 }
