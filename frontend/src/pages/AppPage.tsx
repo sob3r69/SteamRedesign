883d6f4c38980e27a69798dad28a230b794d7ada
sob3r69 <iamnotsob3r@gmail.com>
2024-03-08 22:36:38 +0300

    fix[front]: Some more of layout of app page

--- frontend/src/pages/AppPage.tsx
+++ frontend/src/pages/AppPage.tsx
@@ -28,11 +28,26 @@ const AppPage = () => {
             <WishlistButton type="large" />
           </div>
         </div>
-        <div className="app_page_description"></div>
+        <div className="app_page_description">
+          <div className="row_container">
+            <div className="app_page_description_image"></div>
+            <div className="app_page_description_text"></div>
+          </div>
+        </div>
         <div className="app_page_row">
-          <div className="app_page_content"></div>
+          <div className="app_page_content">
+            <div className="app_page_buy">
+              {/* There is gonna be a map render of buy options */}
+            </div>
+            <div className="app_page_about">
+              {/* There is gonna be about page with screenshots and detailed description */}
+            </div>
+            <div className="app_page_system_requirements"></div>
+          </div>
           <div className="app_page_info"></div>
         </div>
+        <div className="app_page_similar"></div>
+        <div className="app_page_reviews"></div>
       </div>
     );
 };
