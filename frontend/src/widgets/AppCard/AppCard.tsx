b77e654bee641075748ab200491fd640b7af9183
sob3r69 <iamnotsob3r@gmail.com>
2024-03-30 22:03:07 +0300

    fix[front]: Fixed styling of an AppCard. I just forgot about <Link> of tanstack router

--- frontend/src/widgets/AppCard/AppCard.tsx
+++ frontend/src/widgets/AppCard/AppCard.tsx
@@ -4,7 +4,7 @@ import { ErrorHandler } from '..';
 import BigGameCard from './variants/BigGameCard';
 import SmallGameCard from './variants/SmallGameCard';
 import { useAppDataFetch } from '@/entities/app/api';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './AppCard.scss';
 
 type AppCardProps = {
@@ -22,21 +22,21 @@ const AppCard = memo(({ type, gameID }: AppCardProps) => {
     switch (type) {
       case 'big':
         return (
-          <button className="app_card" onClick={() => navigate(`/app/${gameID}`)}>
+          <Link to={`/app/${gameID}`} className="app_card">
             <BigGameCard data={data} />
-          </button>
+          </Link>
         );
       case 'compact':
         return (
-          <button className="app_card" onClick={() => navigate(`/app/${gameID}`)}>
+          <Link to={`/app/${gameID}`} className="app_card">
             <SmallGameCard data={data} type="compact" />
-          </button>
+          </Link>
         );
       case 'wide':
         return (
-          <button className="app_card" onClick={() => navigate(`/app/${gameID}`)}>
+          <Link to={`/app/${gameID}`} className="app_card">
             <SmallGameCard data={data} type="wide" />
-          </button>
+          </Link>
         );
     }
 });
